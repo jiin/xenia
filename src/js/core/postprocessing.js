a65920395b6c85563cbd453b8561a9759c2bf02d
Neri Barnini <neri@mono-grid.com>
2018-01-22 13:17:18 +0100

    - updated .gitignore

--- src/js/core/postprocessing.js
+++ src/js/core/postprocessing.js
@@ -1,20 +1,13 @@
-import * as THREE from 'three'
-
-window.THREE = THREE
-
 import EffectComposer, { RenderPass, ShaderPass, CopyShader } from 'three-effectcomposer-es6'
 
 export default class Postprocessing {
   constructor (settings) {
-    // When you've made your scene, create your composer and first RenderPass
     this._composer = new EffectComposer(settings.renderer)
     this._composer.addPass(new RenderPass(settings.scene, settings.camera))
 
-    // Add shaders! Celebrate!
     // const someShaderPass = new ShaderPass(SomeShader)
     // this._composer.addPass(someShaderPass)
 
-    // And draw to the screen
     const copyPass = new ShaderPass(CopyShader)
     copyPass.renderToScreen = true
     this._composer.addPass(copyPass)
